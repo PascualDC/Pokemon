a0f607fe1832c492ba4055f6277f85e677fb8cd0
PascualDC <pascualdaroca678.alu@stemgranada.com>
2023-09-29 09:10:21 +0200

    Soluciones

--- Pokemons.py
+++ Pokemons.py
@@ -6,5 +6,14 @@ def texto():
                         + "4. Es un Pokémon dragón que puede lanzar llamas ardientes de su boca. Es el último eslabón de una evolución de tres etapas."\
                             + "5. Este Pokémon es un pequeño roedor eléctrico con mejillas rojas y amarillas. Es la mascota más icónica de la franquicia Pokémon.")
     return resultado
-
+def soluciones():
+    resultado = print("1. La serpiente venenosa con un patrón de colores brillantes es Ekans." +
+"2. El Pokémon con una concha dura y cañones de agua en su espalda es Squirtle." +
+" 3. El pájaro grande de tipo volador es Pidgeot." +
+" 4. El dragón que lanza llamas ardientes de su boca es Charizard." +
+"5. El pequeño roedor eléctrico con mejillas rojas y amarillas es Pikachu.")
+    
+    
 print(texto())
+
+print(soluciones())
